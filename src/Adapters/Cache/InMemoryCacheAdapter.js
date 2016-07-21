5640d4e34a37f47ad2fd030e993bf45cfa0e6e14
bertguy <bertguy@users.noreply.github.com>
2016-07-20 22:25:29 -0500

    Redis Memory Adapter Implementation
    Redis Memory Adapter Implementation

--- src/Adapters/Cache/InMemoryCacheAdapter.js
+++ src/Adapters/Cache/InMemoryCacheAdapter.js
@@ -1,34 +1,56 @@
-import {InMemoryCache} from './InMemoryCache';
+var redis = require('redis');
 
 export class InMemoryCacheAdapter {
 
   constructor(ctx) {
-    this.cache = new InMemoryCache(ctx)
+    this.client = redis.createClient('6379','luck-redis.5jot1u.0001.use1.cache.amazonaws.com');
+    this.client.on('connect', function() {
+        console.log('connected');
+    });
   }
 
   get(key) {
     return new Promise((resolve, reject) => {
-      let record = this.cache.get(key);
-      if (record == null) {
-        return resolve(null);
-      }
-
-      return resolve(JSON.parse(record));
+      this.client.get(key, function(error, record) {
+        if (error || record == null) {
+          console.log('record not found');
+          return resolve(null);
+        } else {
+          console.log('record found:');
+          console.log(JSON.parse(record));
+          return resolve(JSON.parse(record));
+        }
+      });
     })
   }
 
-  put(key, value, ttl) {
-    this.cache.put(key, JSON.stringify(value), ttl);
+  put(key, value, ttl = 1800) {
+    this.client.set(key, JSON.stringify(value), function(error, reply) {
+      if (error) {
+        console.log(error);
+      } else {
+        if (ttl > 0 && !isNaN(ttl)) {
+          this.client.expire(key, ttl);
+        }
+        console.log('Record Set');
+      }
+    });
     return Promise.resolve();
   }
 
   del(key) {
-    this.cache.del(key);
+    this.client.del(key, function(error, reply) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Record Deleted');
+      }
+    });
     return Promise.resolve();
   }
 
   clear() {
-    this.cache.clear();
+    //this.cache.clear();
     return Promise.resolve();
   }
 }
