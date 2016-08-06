91ae0a5fccf98fe8688df023d8bee4950c75771e
bertguy <bertguy@users.noreply.github.com>
2016-08-06 00:13:07 -0500

    Removed instanceId from caching

--- src/Adapters/Cache/InMemoryCacheAdapter.js
+++ src/Adapters/Cache/InMemoryCacheAdapter.js
@@ -10,10 +10,6 @@ export class InMemoryCacheAdapter {
     var redisPort = AppCache.get('redisPort');
     var redisURL = AppCache.get('redisURL');
 
-    var id = Math.floor((Math.random() * 10000) + 1);
-
-    this.instanceId = id.toString();
-
     this.client = redis.createClient(redisPort, redisURL);
     this.client.on('connect', function() {
         logger.info('redis connected');
@@ -24,10 +20,10 @@ export class InMemoryCacheAdapter {
     return new Promise((resolve, reject) => {
       this.client.get(key, function(error, record) {
         if (error || record == null) {
-          logger.error('record not found - Key: ' + key + ' from: ' + this.instanceId);
+          logger.error('record not found - Key: ' + key);
           return resolve(null);
         } else {
-          logger.info('record found - key: ' + key + ' from: ' + this.instanceId);
+          logger.info('record found - key: ' + key);
           return resolve(JSON.parse(record));
         }
       });
@@ -42,7 +38,7 @@ export class InMemoryCacheAdapter {
           if (error) {
             logger.error(error);
           } else {
-            logger.info('Record Set with expire.  Key: ' + key + '  ,   expire: ' + expire + ' from: ' + this.instanceId);
+            logger.info('Record Set with expire.  Key: ' + key + '  ,   expire: ' + expire);
           }
         });
     } else {
@@ -50,7 +46,7 @@ export class InMemoryCacheAdapter {
         if (error) {
           logger.error(error);
         } else {
-          logger.info('Record Set without expire - Key: ' + key + ' from: ' + this.instanceId);
+          logger.info('Record Set without expire - Key: ' + key);
         }
       });
     }
@@ -62,7 +58,7 @@ export class InMemoryCacheAdapter {
       if (error) {
         logger.error(error);
       } else {
-        logger.info('Record Deleted - Key: ' + key + ' from: ' + this.instanceId);
+        logger.info('Record Deleted - Key: ' + key);
       }
     });
     return Promise.resolve();
