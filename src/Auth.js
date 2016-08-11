6344c5f18445aba4c5c069dbcf9f773a899e5df6
bertguy <bertguy@users.noreply.github.com>
2016-08-10 23:55:23 -0500

    Finalized getAuthForSessionToken by removing logging

--- src/Auth.js
+++ src/Auth.js
@@ -72,7 +72,6 @@ var getAuthForSessionToken = function({ config, sessionToken, installationId } =
       obj['sessionToken'] = sessionToken;
       config.cacheController.user.put(sessionToken, obj);
       let userObject = Parse.Object.fromJSON(obj);
-      console.log(userObject);
       return new Auth({config, isMaster: false, installationId, user: userObject});
     });
   });
