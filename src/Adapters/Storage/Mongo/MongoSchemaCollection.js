25c0d6dd1e28ee6adec1e3669e41f100937c2a47
bertguy <bertguy@users.noreply.github.com>
2016-09-20 04:53:26 -0500

    Switch back to hardcoded Schema and added swipe class to switch

--- src/Adapters/Storage/Mongo/MongoSchemaCollection.js
+++ src/Adapters/Storage/Mongo/MongoSchemaCollection.js
@@ -110,17 +110,17 @@ class MongoSchemaCollection {
 
   _fetchAllSchemasFrom_SCHEMA() {
 
-    //var hardSchema = AppCache.get('hardSchema');
+    var hardSchema = AppCache.get('hardSchema');
 
-    //return Promise.resolve(hardSchema)
-    //.then(schemas => schemas.map(mongoSchemaToParseSchema));
-
-    return this._collection._rawFind({})
+    return Promise.resolve(hardSchema)
     .then(schemas => schemas.map(mongoSchemaToParseSchema));
+
+    //return this._collection._rawFind({})
+    //.then(schemas => schemas.map(mongoSchemaToParseSchema));
   }
 
   _fechOneSchemaFrom_SCHEMA(name: string) {
-/*
+
     var hardSchema = AppCache.get('hardSchema');
     var hardSchemaIndex = -1;
 
@@ -151,6 +151,9 @@ class MongoSchemaCollection {
         break;
       case 'message':
         hardSchemaIndex = 8;
+        break;
+      case 'swipe':
+        hardSchemaIndex = 9;
     }
 
     return Promise.resolve(hardSchema).then(results => {
@@ -160,14 +163,14 @@ class MongoSchemaCollection {
         throw undefined;
       }
     });
-*/
-    return this._collection._rawFind(_mongoSchemaQueryFromNameQuery(name), { limit: 1 }).then(results => {
-      if (results.length === 1) {
-        return mongoSchemaToParseSchema(results[0]);
-      } else {
-        throw undefined;
-      }
-    });
+
+    //return this._collection._rawFind(_mongoSchemaQueryFromNameQuery(name), { limit: 1 }).then(results => {
+      //if (results.length === 1) {
+        //return mongoSchemaToParseSchema(results[0]);
+      //} else {
+        //throw undefined;
+      //}
+    //});
   }
 
   // Atomically find and delete an object based on query.
