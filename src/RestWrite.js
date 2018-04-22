e6abaf1e38b271fb772b04bb8730ec6026326401
bertguy <bertguy@users.noreply.github.com>
2018-04-22 01:00:10 -0500

    Catch afterSave errors and don't send to client

--- src/RestWrite.js
+++ src/RestWrite.js
@@ -14,6 +14,7 @@ var triggers = require('./triggers');
 var ClientSDK = require('./ClientSDK');
 import RestQuery from './RestQuery';
 import _         from 'lodash';
+import logger    from './logger';
 
 // query and data are both provided in REST API format. So data
 // types are encoded by plain old objects.
@@ -1105,7 +1106,10 @@ RestWrite.prototype.runAfterTrigger = function() {
   this.config.liveQueryController.onAfterSave(updatedObject.className, updatedObject, originalObject);
 
   // Run afterSave trigger
-  return triggers.maybeRunTrigger(triggers.Types.afterSave, this.auth, updatedObject, originalObject, this.config);
+  return triggers.maybeRunTrigger(triggers.Types.afterSave, this.auth, updatedObject, originalObject, this.config)
+    .catch(function(err) {
+      logger.warn('afterSave caught an error', err);
+    })
 };
 
 // A helper to figure out what location this operation happens at.
